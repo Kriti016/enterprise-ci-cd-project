0bcce8be52d0fdc1496f666b76a2124efd89931f
Kriti016 <kritichawla@aoscindia.com>
2026-03-17 13:18:20 +0530

    Update App.java

--- src/main/java/com/demo/App.java
+++ src/main/java/com/demo/App.java
@@ -1,5 +1,7 @@
+package com.demo;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins Project!");
+        System.out.println("Hello World");
     }
 }
